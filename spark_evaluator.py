f44a19c51fa26ae24f04cd59457a0ab943a60ed4
Tymoteusz Machowski <tmachows@gmail.com>
2018-01-18 17:30:22 +0100

    use spark file api

--- spark_evaluator.py
+++ spark_evaluator.py
@@ -5,15 +5,13 @@ from evaluator import evaluate_entity
 
 
 # load data from files to lists
-data_file = open('input.txt', 'r')
-data = json.load(data_file)
 
 rules_file = open('rules.txt', 'r')
 rules = json.load(rules_file)
 
 # prepare spark context
 sc = SparkContext("local", "Rule Evaluation", pyFiles=['evaluator.py'])
-objects = sc.parallelize(data)
+objects = sc.textFile("gs://rule-evaluation-bucket/input.txt")
 
 current_milli_time = lambda: int(round(time.time() * 1000))
 start = current_milli_time()
@@ -24,9 +22,6 @@ result = objects.map(evaluate_entity)
 print "Evaluation complete in: ", current_milli_time() - start, " ms"
             
 # save results
-#with open('output.txt', 'w') as output_file:
-#    json.dump(result, output_file)
-result.saveAsTextFile("output")
+result.saveAsTextFile("gs://rule-evaluation-bucket/output/")
 
-data_file.close()
 rules_file.close()
