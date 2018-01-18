6b4f24d6744c72a1f62f3b932c49f4e3e34d9bd3
DanOgiela <dan.ogiela@gmail.com>
2018-01-18 17:44:32 +0100

    generator improved

--- generator.py
+++ generator.py
@@ -3,7 +3,7 @@ import random
 
 
 number_of_variables_in_one_object = 20
-objects_number = 500000
+objects_number = 500
 all_variables_names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r'
     , 's', 't', 'u', 'v', 'w', 'y', 'z']
 variables = random.sample(all_variables_names, number_of_variables_in_one_object)
@@ -60,8 +60,15 @@ def generate_then_mapping():
     return dict
 
 
+generated_data = generate_data(objects_number)	
+	
 with open('input.txt', 'w') as generated_data_file:
-    json.dump(generate_data(objects_number), generated_data_file)
-
+    json.dump(generated_data, generated_data_file)
+	
+with open('input_spark.txt', 'w') as f:
+    for generated_object in generated_data:
+        f.write(json.dumps(generated_object))
+        f.write("\n")
+	  
 with open('rules.txt', 'w') as generated_rules_file:
     json.dump(generate_rules(rules_number), generated_rules_file)
